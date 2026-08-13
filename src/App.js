f2c8f156a622e89f89aaba85f1e667be1bd4cf41
Ben Ali Aboudou <158784500+Ben-TECHPlayer@users.noreply.github.com>
2026-08-13 18:13:49 +0200

    Nettoyage du projet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// Importer React ainsi que ses routeurs
+import React from "react";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            {/* <Router>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/card" element={<Card/>} />
+                    <Route path="/lineup" element={<Lineup />} />
+
+                    <Route path="/card/teams/database/Portugal" element={<Portugal />} />
+                    <Route path="/card/teams/database/France" element={<France />} />
+                    <Route path="/card/teams/database/Argentine" element={<Argentine />} />
+                    <Route path="/card/teams/database/Spain" element={<Spain />} />
+                    <Route path="/card/teams/database/Germany" element={<Germany />} />
+                    <Route path="/card/teams/database/England" element={<England />} />
+                    <Route path="/card/teams/database/UnitedStates" element={<UnitedStates />} />
+                    <Route path="/card/teams/database/Brazil" element={<Brazil />} />
+                    <Route path="/card/teams/database/Mexico" element={<Mexico />} />
+                </Routes>
+                <Footer/>
+            </Router> */}
+        </div>
+    );
 }
 
 export default App;
