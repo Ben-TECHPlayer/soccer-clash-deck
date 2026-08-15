627b566bf58318012b062df24cba7aada0641fbb
Ben Ali Aboudou <158784500+Ben-TECHPlayer@users.noreply.github.com>
2026-08-15 19:54:32 +0200

    Je suis en train de faire un profil d'équipe

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 // Importer React ainsi que ses routeurs
 import React from "react";
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Importer nos équipes
+import Portugal from "./pages/teams/database/Portugal";
 
 function App() {
     return (
         <div className="App">
-            {/* <Router>
-                <Header />
+            <Router>
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/card" element={<Card/>} />
-                    <Route path="/lineup" element={<Lineup />} />
-
-                    <Route path="/card/teams/database/Portugal" element={<Portugal />} />
-                    <Route path="/card/teams/database/France" element={<France />} />
-                    <Route path="/card/teams/database/Argentine" element={<Argentine />} />
-                    <Route path="/card/teams/database/Spain" element={<Spain />} />
-                    <Route path="/card/teams/database/Germany" element={<Germany />} />
-                    <Route path="/card/teams/database/England" element={<England />} />
-                    <Route path="/card/teams/database/UnitedStates" element={<UnitedStates />} />
-                    <Route path="/card/teams/database/Brazil" element={<Brazil />} />
-                    <Route path="/card/teams/database/Mexico" element={<Mexico />} />
+                    <Route path="/card/teams/database/portugal" element={<Portugal />} />
+                    {/* <Route path="/card/teams/database/brazil" element={<Brazil />} />
+                    <Route path="/card/teams/database/france" element={<France />} />
+                    <Route path="/card/teams/database/argentine" element={<Argentine />} />
+                    <Route path="/card/teams/database/spain" element={<Spain />} />
+                    <Route path="/card/teams/database/germany" element={<Germany />} />
+                    <Route path="/card/teams/database/england" element={<England />} />
+                    <Route path="/card/teams/database/united-states" element={<UnitedStates />} />
+                    <Route path="/card/teams/database/mexico" element={<Mexico />} />
+                    <Route path="/card/teams/database/norway" element={<Norway />} /> */}
                 </Routes>
-                <Footer/>
-            </Router> */}
+            </Router>
         </div>
     );
 }
